a18395f3219c6990260cf378ad9098eb6eb8af46
Ира <89978431+Irocchka@users.noreply.github.com>
2022-10-05 22:34:45 +0300

    Add files via upload

---
+++ Program3_2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
+
+class Car : IEquatable<Car>
+{
+    public string Name;
+    public string Engine;
+    public int MaxSpeed;
+
+    public Car(string Name,string Engine,int MaxSpeed)
+    {
+        this.Name = Name;
+        this.Engine = Engine;
+        this.MaxSpeed = MaxSpeed;
+        Console.WriteLine($"{Name} {Engine} {MaxSpeed}");
+    }
+
+    public bool Equals(Car other)
+    {
+        return this == other;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
+class CarsCatalog
+{
+    public List<Car> cars = new List<Car>();//List только для строк
+    public CarsCatalog(params Car[] car)
+    {
+        for(int i = 0; i < car.Length; i++)
+        {
+            this.cars.Add(car[i]);
+        }
+    }
+    public string this[int index]
+    {
+        get => $"{cars[index].Name} ; { cars[index].Engine}";
+    }
+
+}
+
+ class Program
+    {
+        static void Main(string[] args)
+        {
+        CarsCatalog c1 = new CarsCatalog(new Car("Audi a4", "V6", 180), new Car("BMW x6", "V3", 230),new Car("Mers","V1",200));
+        string mrr=c1[0].ToString();
+        
+        for (int i = 0; i < 3; i ++)
+        {
+            Console.WriteLine(c1[i].ToString());
+        }
+        }
+  }
+
---
+++ Program3_3.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace lr3_3
+{
+    class Currency
+    {
+        public double Value;
+    }
+    class CurrencyUSD : Currency
+    {
+        static public double ieur=8;
+        static public double irub=5;
+
+        public CurrencyUSD(double val)
+        {
+            this.Value = val;
+        }
+
+        public static explicit operator CurrencyUSD(CurrencyEUR eur)
+        {
+            return new CurrencyUSD(eur.Value * ieur);
+        }
+        public static explicit operator CurrencyUSD(CurrencyRUB rub)
+        {
+            return new CurrencyUSD(rub.Value * irub);
+        }
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+    class CurrencyEUR : Currency
+    {
+        static public double iirub=3;
+        static public double iusd=2;
+        public CurrencyEUR(double val)
+        {
+            this.Value = val;
+        }
+        public static explicit operator CurrencyEUR(CurrencyUSD usd)
+        {
+            return new CurrencyEUR(usd.Value * iusd);
+        }
+        public static explicit operator CurrencyEUR(CurrencyRUB rub)
+        {
+            return new CurrencyEUR(rub.Value * iirub);
+        }
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+    class CurrencyRUB: Currency
+    {
+        static public double iieur=9;
+        static public double iiusd=7;
+        public CurrencyRUB(double val)
+        {
+            this.Value = val;
+        }
+        public static explicit operator CurrencyRUB(CurrencyEUR eur)
+        {
+            return new CurrencyRUB(eur.Value * iieur);
+        }
+        public static explicit operator CurrencyRUB(CurrencyUSD usd)
+        {
+            return new CurrencyRUB(usd.Value * iiusd);
+        }
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = 100;
+            Console.WriteLine($"rub: {money}");
+            CurrencyRUB money_rub = new CurrencyRUB(money);
+            CurrencyUSD money_usd = (CurrencyUSD)money_rub;
+            CurrencyEUR money_eur = (CurrencyEUR)money_rub;
+            Console.WriteLine($"usd: {money_usd}");
+            Console.WriteLine($"eur: {money_eur}");
+        }
+    }
+}
